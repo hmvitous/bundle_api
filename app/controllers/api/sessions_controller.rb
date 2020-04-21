0d9fef0d2f69a7e463b8ed181603f938dd501133
Anish2504 <anish.kanswal@gmail.com>
2020-04-21 15:06:27 +0200

    added test for village if you are not in stockholm or gothenburg
    Co-authored-by:Hunter<hmvitous@yahoo.com>

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,12 +3,12 @@ class Api::SessionsController < ApplicationController
     lat=params[:location][:latitude].to_f
     long=params[:location][:longitude].to_f
     results = Geocoder.search([lat, long])
-
+    edition = (results.first.city.match? /Stockholm|Gothenburg/) ? results.first.city : 'Country Wide'
     render json: {session: 
     {location:
     {latitude: lat,
     longitude: long},
-    edition: results.first.city }
+    edition: edition }
   }
   end
 end
